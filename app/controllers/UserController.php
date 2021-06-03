86de776013c46858a03d5c43a59ac7c94798ebc4
seryy366 <truhachev.sergei@yandex.ru>
2021-06-03 16:42:31 +0300

    first commit

---
+++ app/controllers/UserController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\User;
+
+class UserController extends AppController {
+
+    public function signupAction(){
+        if(!empty($_POST)){
+            $user = new User();
+            $data = $_POST;
+            $user->load($data);
+            if(!$user->validate($data) || !$user->checkUnique()){
+                $user->getErrors();
+                $_SESSION['form_data'] = $data;
+            }else{
+                $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
+                if($user->save('user')){
+                    $_SESSION['success'] = 'Пользователь зарегистрирован';
+                }else{
+                    $_SESSION['error'] = 'Ошибка!';
+                }
+            }
+            redirect();
+        }
+        $this->setMeta('Регистрация');
+    }
+
+    public function loginAction(){
+        if(!empty($_POST)){
+            $user = new User();
+            if($user->login()){
+                $_SESSION['success'] = 'Вы успешно авторизованы';
+                redirect('/user/cabinet');
+            }else{
+                $_SESSION['error'] = 'Логин/пароль введены неверно';
+            }
+            redirect();
+        }
+        $this->setMeta('Вход');
+    }
+
+    public function logoutAction(){
+        if(isset($_SESSION['user'])) unset($_SESSION['user']);
+        redirect();
+    }
+
+    public function cabinetAction(){
+        if(!User::checkAuth()) redirect();
+        $this->setMeta('Личный кабинет');
+    }
+
+    public function editAction(){
+        if(!User::checkAuth()) redirect('/user/login');
+        if(!empty($_POST)){
+            $user = new \app\models\admin\User();
+            $data = $_POST;
+            $data['id'] = $_SESSION['user']['id'];
+            $data['role'] = $_SESSION['user']['role'];
+            $user->load($data);
+            if(!$user->attributes['password']){
+                unset($user->attributes['password']);
+            }else{
+                $user->attributes['password'] = password_hash($user->attributes['password'], PASSWORD_DEFAULT);
+            }
+            if(!$user->validate($data) || !$user->checkUnique()){
+                $user->getErrors();
+                redirect();
+            }
+            if($user->update('user', $_SESSION['user']['id'])){
+                foreach($user->attributes as $k => $v){
+                    if($k != 'password') $_SESSION['user'][$k] = $v;
+                }
+                $_SESSION['success'] = 'Изменения сохранены';
+            }
+            redirect();
+        }
+        $this->setMeta('Изменение личных данных');
+    }
+
+    public function ordersAction(){
+        if(!User::checkAuth()) redirect('/user/login');
+        $orders = \R::findAll('order', 'user_id = ?', [$_SESSION['user']['id']]);
+        $this->setMeta('История заказов');
+        $this->set(compact('orders'));
+    }
+
+}
---
+++ config/params.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'admin_email' => 'matroskin978@gmail.com',
+    'shop_name' => 'Магазин ishop2.loc',
+    'pagination' => 3,
+    'smtp_host' => 'smtp.ukr.net',
+    'smtp_port' => '2525',
+    'smtp_protocol' => 'ssl',
+    'smtp_login' => 'testishop2@ukr.net',
+    'smtp_password' => 'testishop_2',
+    'img_width' => 125,
+    'img_height' => 200,
+    'gallery_width' => 700,
+    'gallery_height' => 1000,
+    'ik_key' => 'uc18larViw9UYbsa',
+    'ik_id' => '59bf6fc03d1eaf2e638b4569',
+];
